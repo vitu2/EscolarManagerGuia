d2e76135c34198d9bbe5992ecd4e64a20711b99e
Victor Emanuel <95113352+vitu2@users.noreply.github.com>
2023-02-07 00:05:13 -0300

    alterando as imagens e diretorios, mudando estrutura do html

--- src/components/gestão financeira/ContasContabeis/ContasContabeis.jsx
+++ src/components/gestão financeira/ContasContabeis/ContasContabeis.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import img1 from '../../../assets/image/cadastroBanco.png'
-import img2 from '../../../assets/image/infoBancarias.png';
-import img3 from '../../../assets/image/gcGeral.png';
-import img4 from '../../../assets/image/cc.png';
-import img5 from '../../../assets/image/NewCC.png';
+import cadastroBanco from '../../../assets/image/cadastroBanco.png';
+import infoBancaria from '../../../assets/image/infoBancarias.png';
+import gcGeral from '../../../assets/image/gcGeral.png';
+import cc from '../../../assets/image/cc.png';
+import NewCC from '../../../assets/image/NewCC.png';
 
 function ContasContabeis() {
   return (
@@ -11,27 +11,25 @@ function ContasContabeis() {
       <h1>Gestão Financeira:Introdução</h1>
 
       <h2>Fluxo Para Criação da Conta Contabil</h2>
+      <p>
+        Primeiramente devemos criar um banco (cadastrar um banco) segue a imagem
+        de exemplo:
+      </p>
       <figure>
-        <p>
-          Primeiramente devemos criar um banco (cadastrar um banco) segue a
-          imagem de exemplo:
-        </p>
-        <img src={img1} />
+        <img src={cadastroBanco} />
       </figure>
 
       <p>
         Devemos clicar em novo, abrira uma tela de cadastro de banco, segue a
         imagen de exemplo:
       </p>
-      <p>
-        <figure>
-          <img src={img2} />
-          <figcaption>
-            Nesta tela informaremos os dados bancarios do cliente (será feito
-            pela implantação mas eventualmente faremos suporte)
-          </figcaption>
-        </figure>
-      </p>
+      <figure>
+        <img src={infoBancaria} />
+        <figcaption>
+          Nesta tela informaremos os dados bancarios do cliente (será feito pela
+          implantação mas eventualmente faremos suporte)
+        </figcaption>
+      </figure>
 
       <p>
         Neste ponto já podemos usar esse banco, no caso do exemplo temos mais de
@@ -44,7 +42,7 @@ function ContasContabeis() {
         abriremos a seguinte tela:
       </p>
       <figure>
-        <img src={img3} />
+        <img src={gcGeral} />
         <figcaption>
           Nesta tela fazemos o cadastro de grupos, quando clicamos em novo grupo
           pai, criamos uma conta contabil que sera usada em no novo grupo,
@@ -56,23 +54,23 @@ function ContasContabeis() {
         Neste momento podemos já podemos criar a nossa conta contabil, vamos
         realizar o cadastro agora segue a imagem a seguir de como fazemos isso:
       </p>
+      <p>
+        Nesta tela criaremos a nossa conta contabil, aqui podemos começar a
+        organizar o financeiro da escola, claro que para ter um bom relatorio é
+        necessario ter uma boa organização e alimentar as informações seguindo
+        os fluxos de maneira correta, nesta tela podemos ver algumas informações
+        interressantes na coluna descrição de conta, vemos que temos uma conta
+        contabil para finalidades diferentes e manter a organização como por
+        exemplo temo uma CC(conta contabil), para a conta de agua, recebimento
+        de matricula e mensalidade, ficara a cargo do suporte decidir quantas CC
+        terão ou serão necessario para o controle.
+      </p>
       <figure>
-        <img src={img4} />
-        <p>
-          Nesta tela criaremos a nossa conta contabil, aqui podemos começar a
-          organizar o financeiro da escola, claro que para ter um bom relatorio
-          é necessario ter uma boa organização e alimentar as informações
-          seguindo os fluxos de maneira correta, nesta tela podemos ver algumas
-          informações interressantes na coluna descrição de conta, vemos que
-          temos uma conta contabil para finalidades diferentes e manter a
-          organização como por exemplo temo uma CC(conta contabil), para a conta
-          de agua, recebimento de matricula e mensalidade, ficara a cargo do
-          suporte decidir quantas CC terão ou serão necessario para o controle.
-        </p>
+        <img src={cc} />
       </figure>
 
       <figure>
-        <img src={img5} />
+        <img src={NewCC} />
         <figcaption>
           Nesta tela posso escolher o grupo que foi criado mais acima no
           cadastro de grupos de conta contabil depois de adicionarmos o grupo
@@ -82,10 +80,12 @@ function ContasContabeis() {
 
       <h3>Resalvas Para a Versão Web</h3>
       <p>
-        Necessario colocar o nome do banco, porque <strong>esta diferente dessa tela,
-        não mostra o nome na versão web.</strong>
+        Necessario colocar o nome do banco, porque{' '}
+        <strong>
+          esta diferente dessa tela, não mostra o nome na versão web.
+        </strong>
       </p>
-      <img src={img4} />
+      <img src={cc} />
       <p>
         Como vemos na imagem acima, na descrição da conta na pratica não devia
         estar o nome do banco, porque na coluna a frente está me falando em qual
