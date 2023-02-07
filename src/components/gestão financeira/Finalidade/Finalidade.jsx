a04fdf24071d3288e6592631b95b4be3fd6dcd60
Victor Emanuel <95113352+vitu2@users.noreply.github.com>
2023-02-06 23:37:07 -0300

    alterando as imagens e diretorios

--- src/components/gestão financeira/Finalidade/Finalidade.jsx
+++ src/components/gestão financeira/Finalidade/Finalidade.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import img12 from '../../../assets/image/finalidade.png'
+// import img12 from '../../../assets/image/finalidade.png'
 import img13 from '../../../assets/image/finalidadeCadas.png'
 
 function Finalidade() {
@@ -8,9 +8,9 @@ function Finalidade() {
       <h1>Finalidades:Introdução</h1>
 	  <p><strong>O que é a finalidade:</strong> A finalidade seve para apontar sobre o que é aquela duplicata, na imagem vemos alguns exemplos
 	  como matricula e mensalidade</p>
-	  <figure>
+	  {/* <figure>
 		<img src={img12} />
-	  </figure>
+	  </figure> */}
 	  <p><strong>Criar uma nova finalidade</strong> para podermos criar uma nova finalidade clicar em novo e sera a aberto a seguinte tela</p>
 	  	  <figure>
 		<img src={img13} />
