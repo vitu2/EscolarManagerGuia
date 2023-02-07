04a01dad14856d370ca62bd1ceb3ffe7c1fdf69b
Victor Emanuel <95113352+vitu2@users.noreply.github.com>
2023-02-06 23:51:22 -0300

    alterando as imagens e diretorios

--- src/components/gestão financeira/Finalidade/Finalidade.jsx
+++ src/components/gestão financeira/Finalidade/Finalidade.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import img12 from '../../../assets/image/finalidade.png'
-// import img13 from '../../../assets/image/finalidadeCadas.png'
+import img12 from '../../../assets/image/finalidade.png'
+import img13 from '../../../assets/image/finalidadeCadas.png'
 
 function Finalidade() {
   return (
@@ -8,13 +8,13 @@ function Finalidade() {
       <h1>Finalidades:Introdução</h1>
 	  <p><strong>O que é a finalidade:</strong> A finalidade seve para apontar sobre o que é aquela duplicata, na imagem vemos alguns exemplos
 	  como matricula e mensalidade</p>
-	  {/* <figure>
+	  <figure>
 		<img src={img12} />
-	  </figure> */}
+	  </figure>
 	  <p><strong>Criar uma nova finalidade</strong> para podermos criar uma nova finalidade clicar em novo e sera a aberto a seguinte tela</p>
-	  	  {/* <figure>
+	  	  <figure>
 		<img src={img13} />
-	  </figure> */}
+	  </figure>
 	  <p>Aqui ja podemos cadastrar uma nova finalidade e assim podemos fazer o fluxo para a geração do financeiro do aluno.</p>
     </div>
   )
