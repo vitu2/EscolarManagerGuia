0b9d2264efe1400c41f46dacb33e5342ae182970
tresfg4323 <vitorfacundes123@gmail.com>
2023-02-10 18:00:07 -0300

    alterando os dados

--- src/components/gestãoFinanceira/Cartão/Cartão.jsx
+++ src/components/gestãoFinanceira/Cartão/Cartão.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function Cartão() {
     return (
         <div>
@@ -10,8 +11,21 @@ function Cartão() {
 
             <figure>
                 <img src="https://images2.imgbox.com/8f/8e/iKSENu69_o.png"/>
-                <figcaption></figcaption>
+                <figcaption>Antes de vir para essa tela criar as contas contabeis referentes para o cartão</figcaption>
             </figure>
+        {/* MELHORAR COM O REACT-IMAGE */}
+            <p>Exemplos das Contas Contabeis abertas para a criação do cartão</p>
+            <img src="../../../assets/image temp/CCartão.PNG" alt="" />
+            <img src="../../../assets/image temp/CardDebitCredit.PNG" alt="" />
+
+            <p>Mostrado os exemplos acima podemos preencher os campos de forma correta, como o exemplo abaixo</p>
+            <img src="../../../assets/image temp/Cartão.PNG" alt="" />
+
+            <p>Depois de preenchido vamos definir os meios de pagamento do cartão, como por exemplo 12 vezes sem juros
+                com taxa, a vista sem taxa, podemos definir tudo isso nesse campo
+            </p>
+
+            <img src="../../../assets/image temp/TiposPagamentoCard.PNG" alt="" />
 
         </div>
     )
