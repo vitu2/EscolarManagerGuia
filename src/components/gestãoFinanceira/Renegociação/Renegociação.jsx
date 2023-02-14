a0ea1b59c3bf1f735feda85df01692fb13d86480
tresfg4323 <vitorfacundes123@gmail.com>
2023-02-14 18:00:29 -0300

    base

--- src/components/gestãoFinanceira/Renegociação/Renegociação.jsx
+++ src/components/gestãoFinanceira/Renegociação/Renegociação.jsx
@@ -1,1 +1,16 @@
+import React from 'react'
 
+function Renegociação() {
+  return (
+    <div>
+      <h1>Renegociação: Introdução</h1>
+      <p>Antes de qualquer coisa não vamos usar a tela de renegociação que aparece no resumo financeiro do aluno</p>
+      <figure>
+        <img src="../../../assets/img temp/ReneResumoFinanceiro.PNG" alt="" />
+      </figure>
+      <p>Na imagem acima</p>
+    </div>
+  )
+}
+
+export default Renegociação
