85d11da34e97d33834a67e7cf287dda1595808da
tresfg4323 <vitorfacundes123@gmail.com>
2023-02-16 17:55:56 -0300

    base renegociação

--- src/components/gestãoFinanceira/Renegociação/Renegociação.jsx
+++ src/components/gestãoFinanceira/Renegociação/Renegociação.jsx
@@ -4,11 +4,26 @@ function Renegociação() {
   return (
     <div>
       <h1>Renegociação: Introdução</h1>
-      <p>Antes de qualquer coisa não vamos usar a tela de renegociação que aparece no resumo financeiro do aluno</p>
+      <p>Temos duas telas de renegociação, a imagem abaixo é tela que nos encontramos na tela do resumo
+        financeiro, mas quando usamos essa renegociação nos mantemos as duplicatas originais, quando mantemos as 
+        duplicatas é necessario fazer o cancelamento, e o responsavel financeiro não pagar as duplicatas antigas. 
+      </p>
       <figure>
         <img src="../../../assets/img temp/ReneResumoFinanceiro.PNG" alt="" />
       </figure>
-      <p>Na imagem acima</p>
+      <p>Logo nesta imagem abaixo, podemos ver que temos um outra tela de renegociação em gestão financeira</p>
+      <figure>
+        <img src="../../../assets/img temp/TelaRenegociação.png" alt="" />
+      </figure>
+      <p>Nesta tela de renegociação, fazemos a pesquisa pelas duplicatas, ou da forma mais facil podemos pesquisar pelo
+        aluno e carregar as duplicatas, clicamos em todos para selecionar todas as duplicatas, mas onde está a diferença?
+        na tela que estamos a renegociação é por cancelamento, ou seja as duplicatas anteriores são canceladas e assim,
+        tornando assim um fluxo mais facil.
+      </p>
+      <p>Aqui vemos as imagens abaixo, as difenças das telas</p>
+      <figure>
+        <img src="../../../assets/img temp/DiferentesTelaRene.PNG" alt="" />
+      </figure>
     </div>
   )
 }
